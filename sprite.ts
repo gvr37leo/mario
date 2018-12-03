4f81cceab9eb91157bf4709fa07b8b6e147e6e49
Paul Boon <gvr37.p.l.boon@gmail.com>
2018-12-03 16:59:02 +0100

    p

--- sprite.ts
+++ sprite.ts
@@ -1,9 +1,11 @@
 class Sprite{
     
     renderer:SpriteRenderer
+    size:Vector
 
     constructor(public imageData:ImageData){
-        this.renderer = new SpriteRenderer(this,[255,0,0],0)
+        this.size = new Vector(imageData.width,imageData.height)
+        this.renderer = new SpriteRenderer(this,[255,0,0,255],0)
     }
 
     static fromString(url:string):Promise<Sprite>{
@@ -47,21 +49,61 @@ class Sprite{
 }
 
 class SpriteRenderer{
-
-
+    center: Vector
+    temp:Vector = new Vector(0,0)
+    matrix:number[][]
+    rotation:number
+    private matrixID:number[][] = [
+        [ 1, 0],
+        [ 0, 1],
+    ]
+    private matrix90:number[][] = [
+        [0,-1],
+        [1, 0],
+    ]
+    private matrix180:number[][] = [
+        [-1, 0],
+        [ 0,-1],
+    ]
+    private matrix270:number[][] = [
+        [ 0, 1],
+        [-1, 0],
+    ]
+    private matrices:number[][][] = [this.matrixID,this.matrix90,this.matrix180,this.matrix270]
 
     constructor(public sprite:Sprite,public modifiercolor:number[],public strength:number){
-
+        this.center = this.sprite.size.c().sub(Vector.one).scale(0.5)
+        this.matrix = this.matrix180
     }
 
     shader(relpos:Vector):number[]{
-        var color = this.sprite.getPixel(relpos)
+        this.temp.overwrite(relpos)
+        var incolor = this.sprite.getPixel( this.rotateAround(this.temp, this.center))
+        // var incolor = this.sprite.getPixel(this.temp)
+        var outcolor = [0,0,0,0]
+        
 
-        for(var i = 0; i < 3; i++){
-            color[i] = lerp(color[i],this.modifiercolor[i],this.strength) 
+        for(var i = 0; i < 4; i++){
+            
+            // outcolor[i] = lerp(incolor[i],this.modifiercolor[i],this.strength) 
+            outcolor[i] = incolor[i]
         }
 
-        return color
+        return outcolor
+    }
 
+    rotateAround(v:Vector,center:Vector){
+        v.sub(center)
+        this.rotate90(v,1)
+        return v.add(center)
+    }
+
+    rotate90(v:Vector,times:number){
+        this.matrix = this.matrices[times % this.matrices.length]
+        var xp = this.matrix[0][0] * v.x + this.matrix[1][0] * v.y
+        var yp = this.matrix[0][1] * v.x + this.matrix[1][1] * v.y
+        v.x = xp
+        v.y = yp
+        return v
     }
 }
